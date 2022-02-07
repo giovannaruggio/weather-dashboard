d687016adab8d6d569b3361cff9d9b555b95b8c2
Giovanna Ruggio <giovannaruggio@gmail.com>
2022-02-07 15:41:32 -0500

    File structure

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+
+var APIKey = "&units=imperial&appid=f950a94f4fd7409e3c556c7a6d058cd0";
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=";
+
+var cityArray = JSON.parse(localStorage.getItem("cities")) || [];
+
+
