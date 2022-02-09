5667df2f6682f38aa1209997da9be233320ef7eb
Giovanna Ruggio <giovannaruggio@gmail.com>
2022-02-09 16:08:29 -0500

    Additional commenting to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,8 @@ var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=';
 var today = moment(). format('L');
 var cityHistory = [];
 
-$('#city-form').submit(function(event){
-	event.preventDefault()
-
+// renderCities function to save searched cities to page
+function renderCities() {
 	var city = $('#city-input').val().trim();
 	currentWeather(city);
 
@@ -15,23 +14,42 @@ $('#city-form').submit(function(event){
 		var searchedCity = `<button class="city-name list-group-item">${city}</button>`;
 
 		$('.list-group').append(searchedCity);
-	}
 
+	};
+};
+
+// Get previously searched cities from storage
+function inti() {
+	var storedCities = JSON.parse(localStorage.getItem('city'));
+	if (storedCities !== null) {
+		city = storedCities;
+	};
+	renderCities();
+};
+
+
+// Event listener to submit search
+$('#city-form').submit(function(event){
+	event.preventDefault();
+
+	renderCities();
+
+// Save search history
 	localStorage.setItem('city', JSON.stringify(cityHistory));
-	console.log(cityHistory);
 });
 
+// currentWeather function to render & append current weather data to page
 function currentWeather(city) {
 	var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + APIKey;
-
+// Request current weather data from ^ URL ^ based on city
 	$.ajax({
 		url: queryURL,
 		method: 'GET'
-// Promise -- sending back data if data is there
+// Promise to send back data if data is there to send
 	}).then(function(currentWeatherCity) {
 		console.log(currentWeatherCity);
 
-		$('.weather')
+		$('.weather');
 		$('.details').empty();
 
 		var icon = currentWeatherCity.weather[0].icon;
@@ -43,11 +61,11 @@ function currentWeather(city) {
                 <p class="wind"> Wind: ${currentWeatherCity.wind.speed} mph</p>`;
 
 		$('.details').append(city);
-
+		// Aquire uv index through latitude & longitude 
 		var latitude = currentWeatherCity.coord.lat;
 		var longitude = currentWeatherCity.coord.lon;
 		var queryURLUVI = 'https://api.openweathermap.org/data/2.5/uvi?lat=' + latitude + '&lon=' + longitude + '&appid=' + APIKey;
-
+	// Request uv index data from ^ URL ^
 		$.ajax({
 			url: queryURLUVI,
 			method: 'GET'
@@ -61,7 +79,7 @@ function currentWeather(city) {
 			$('.details').append(uviDetail);
 
 			futureWeather(uviCity.lat, uviCity.lon);
-
+			//Render attribute based on uv index 
 			if (uvi >= 0 && uvi <= 4) {
 				$('.uv-index').attr('class', 'badge-success');
 			} else if (uvi >=4 && uvi <= 8) {
@@ -72,11 +90,12 @@ function currentWeather(city) {
 		});
 	});
 
-}
+};
 
+// futureWeather function render & append 5 day forecast weather data to page
 function futureWeather(latitude, longitude) {
 	var forecastURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&units=imperial&exclude=current,minutely,hourly,alerts&appid=' + APIKey;
-
+// Request uv index data from ^ URL ^
 	$.ajax({
 		url: forecastURL,
 		method: 'GET'
@@ -84,16 +103,16 @@ function futureWeather(latitude, longitude) {
 		console.log(forecastCity);
 
 		$('.five-day').empty();
-		
+		// Loop through five days
 		for (let i = 1; i <6; i++) {
 			var cityForecast = {
 				date: forecastCity.daily[i].dt,
 				icon: forecastCity.daily[i].weather[0].icon,
                 temp: forecastCity.daily[i].temp.day,
                 humidity: forecastCity.daily[i].humidity
-			}
+			};
 
-
+			// Moment() to display accurate date
 			var today = moment.unix(cityForecast.date).format('MM/DD/YYYY');
 			var iconURL = `<img src="https://openweathermap.org/img/w/${cityForecast.icon}.png" alt=""/>`;
 
@@ -110,12 +129,6 @@ function futureWeather(latitude, longitude) {
 
 			$('.five-day').append(forecast);
 
-		}
+		};
 	});
-}
-
-// function homeScreen(){
-//     $('#city-input').defaultValue = "Albuquerque";
-//     currentWeather();
-// 	futureWeather();
-// }
+};
